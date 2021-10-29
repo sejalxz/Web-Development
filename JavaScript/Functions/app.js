3b5f3364c73330cfd6924cb0b973b399d770822c
Sejal Dahake <sejaldahake2001@gmail.com>
2021-10-29 11:01:17 +0530

    Add functions - app.js

---
+++ JavaScript/Functions/app.js
@@ -0,0 +1,22 @@
+function singSong() {
+    console.log("Happier")
+}
+
+singSong();
+
+function greet(person) {
+    console.log(`Hii, ${person}!!`);
+}
+
+greet("Sejal");
+
+function greetfFullName(fName, lName) {
+    console.log(`Hii, ${fName + " " + lName[0]}!!`);
+}
+
+greetfFullName("Sejal", "Dahake");
+
+function add(x, y) {
+    let sum = x + y;
+    return sum;
+}
