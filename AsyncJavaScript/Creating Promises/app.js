d2ec9e2060e7368c4ef9aebbd546ef3c6c2841c3
Sejal Dahake <sejaldahake2001@gmail.com>
2021-12-12 10:44:28 +0530

    Add Creating Promises

---
+++ AsyncJavaScript/Creating Promises/app.js
@@ -0,0 +1,65 @@
+const fakeRequest= (url)=>{
+    return new Promise((resolve,reject)=>{
+        const rand = Math.random();
+        setTimeout(()=>{
+            if(rand<0.7){
+                resolve('Here is your fake data!');
+            }
+            else{
+                reject("Connection Timeout!")
+            }
+        },1000)
+    })
+}
+
+fakeRequest('dogs.com')
+.then((data)=>{
+    console.log("Done!");
+    console.log(data);
+})
+.catch((err)=>{
+    console.log("OH NO! ERROR");
+    console.log(err);
+})
+
+// const delayedColorChange = (newColor, delay, doNext) => {
+//     setTimeout(() => {
+//         document.body.style.backgroundColor = newColor;
+//         doNext && doNext();
+//     }, delay)
+// }
+
+// delayedColorChange('red', 1000, () => {
+//     delayedColorChange('orange', 1000, () => {
+//         delayedColorChange('yellow', 1000, () => {
+//             delayedColorChange('green', 1000, () => {
+//                 delayedColorChange('blue', 1000, () => {
+//                     delayedColorChange('indigo', 1000, () => {
+//                         delayedColorChange('violet', 1000, () => {
+
+//                         })
+//                     })
+//                 })
+//             })
+//         })
+//     })
+// });
+
+
+const delayedColorChange = (color, delay) => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            document.body.style.backgroundColor = color;
+            resolve();
+        }, delay)
+    })
+}
+
+
+delayedColorChange('red', 1000)
+    .then(() => delayedColorChange('orange', 1000))
+    .then(() => delayedColorChange('yellow', 1000))
+    .then(() => delayedColorChange('green', 1000))
+    .then(() => delayedColorChange('blue', 1000))
+    .then(() => delayedColorChange('indigo', 1000))
+    .then(() => delayedColorChange('violet', 1000))
