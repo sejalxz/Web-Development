6bf11d3ccdd61e7601874db42b789cc7d541898b
Sejal Dahake <sejaldahake2001@gmail.com>
2022-01-13 17:41:21 +0530

    Installing Global Packages

--- Node JS/Modules_NPM/Jokester/index.js
+++ Node JS/Modules_NPM/Jokester/index.js
@@ -2,6 +2,8 @@ const jokes = require('give-me-a-joke');
 const colors = require('colors');
 // console.dir(jokes);
 
+const cowsay = require('cowsay')
+
 jokes.getRandomDadJoke (function(joke) {
      console.log(joke.rainbow);
 });
